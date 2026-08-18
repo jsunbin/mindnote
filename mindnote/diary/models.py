9b42d2ba0c828d3271ff4e3819d2b1a0efc4b791
jsunbin <jsnb6n@gmail.com>
2026-08-18 15:08:06 +0900

    [feat] Page 모델 생성 및 마이그레이션 적용

--- mindnote/diary/models.py
+++ mindnote/diary/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    # 제목, 내용, 감정 상태, 감정 점수, 작성일
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    feeling = models.CharField(max_length=80)
+    score = models.IntegerField()
+    dt_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
