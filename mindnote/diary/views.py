e1f033c2feacb7e1c5309b05ff882eef58ea66be
jsunbin <jsnb6n@gmail.com>
2026-08-20 16:59:12 +0900

    [design] 정적 파일(CSS, 이미지) 적용 및 전체 템플릿 구조 개편

--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -3,14 +3,14 @@ from .models import Page
 
 # Create your views here.
 def page_list(request):
-    # 변수 object_list에 모든 데이터 가져오기
     object_list = Page.objects.all()
-    context = {"page_list": object_list}
-    return render(request,'diary/page_list.html', context)
+    return render(request, 'diary/page_list.html', {'object_list': object_list})
 
 
 def page_detail(request, page_id):
-    page = Page.objects.get(id=page_id)
-    context = {"page" : page}
-    return render(request, 'diary/page_detail.html', context)
+    object = Page.objects.get(id=page_id)
+    return render(request, 'diary/page_detail.html', {'object': object})
 
+
+def info(request):
+    return render(request, 'diary/info.html')
