20acb25874878e31537a1c988b2f58cccd150b0c
jsunbin <jsnb6n@gmail.com>
2026-08-24 00:46:59 +0900

    [feat] 커스텀 유효성 검사(Validator) 추가 및 적용

--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -20,10 +20,10 @@ def info(request):
 def page_create(request):
     if request.method == "POST":
         page_form = PageForm(request.POST)
-        new_page = page_form.save()
-
-        return redirect('page-detail', page_id=new_page.id)
+        if page_form.is_valid():
+            new_page = page_form.save()
+            return redirect('page-detail', page_id=new_page.id)
     else:
         page_form = PageForm()
         
-        return render(request, 'diary/page_form.html', {'form' : page_form})
+    return render(request, 'diary/page_form.html', {'form' : page_form})
