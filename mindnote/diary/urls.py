e26ea05b5add99b993c1b1ccf20f066005db43d9
jsunbin <jsnb6n@gmail.com>
2026-08-27 01:20:18 +0900

    [feat] 인덱스 페이지 추가

--- mindnote/diary/urls.py
+++ mindnote/diary/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('diary', views.page_list, name='page-list'),
     path('diary/info/', views.info, name='info'),
     path('diary/write/', views.page_create, name='page-create'),
