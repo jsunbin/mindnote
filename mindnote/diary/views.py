3131c7ab1e985e992d6bcccabdf83aff0eed4158
jsunbin <jsnb6n@gmail.com>
2026-09-07 23:49:42 +0900

    [feat] 게시글 리스트 페이지네이션 기능 구현

--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
 from .models import Page
 from .forms import PageForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 def page_list(request):
     object_list = Page.objects.all()
-    return render(request, 'diary/page_list.html', {'object_list': object_list})
+    paginator = Paginator(object_list, 8)
+    curr_page_number = request.GET.get('page')
+
+    if curr_page_number is None:
+        curr_page_number = 1
+
+    page = paginator.page(curr_page_number)
+
+    return render(request, 'diary/page_list.html', {'page': page})
 
 
 def page_detail(request, page_id):
