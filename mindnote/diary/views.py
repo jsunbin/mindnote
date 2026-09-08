2e72db048e938461638ddc7202b466fc8e8438f4
jsunbin <jsnb6n@gmail.com>
2026-09-08 18:32:48 +0900

    [refactor] page_detail 뷰를 DetailView로 리팩토링

--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse
 from .models import Page
 from .forms import PageForm
@@ -16,9 +16,11 @@ class PageListView(ListView):
     page_kwarg = 'page'
 
 
-def page_detail(request, page_id):
-    object = Page.objects.get(id=page_id)
-    return render(request, 'diary/page_detail.html', {'object': object})
+class PageDetailView(DetailView):
+    model = Page
+    template_name = 'diary/page_detail.html'
+    pk_url_kwarg = 'page_id'
+    # context_object_name = 'page'
 
 
 def info(request):
